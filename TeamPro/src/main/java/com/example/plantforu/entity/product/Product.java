329f1e36d78ed828fabe683b354101a577201a52
MrCharon <sungupark83@gmail.com>
2021-10-20 15:02:34 +0900

    product 기본 생성

---
+++ TeamPro/src/main/java/com/example/plantforu/entity/product/Product.java
@@ -0,0 +1,74 @@
+package com.example.plantforu.entity.product;
+
+import java.util.*;
+
+import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+
+import org.hibernate.annotations.*;
+
+import com.example.plantforu.entity.*;
+
+import lombok.*;
+import lombok.experimental.*;
+
+@Getter
+@Setter
+@ToString(exclude="ctgno")
+@EqualsAndHashCode(callSuper=false)
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Accessors(chain=true)
+@Entity
+@DynamicUpdate
+public class Product extends BaseCreateTimeEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator="product_seq")
+	@SequenceGenerator(name="product_seq", sequenceName="product_seq", allocationSize=1)
+	private String pno;
+	
+	@Lob
+	private String pdetail;
+	
+	@Column(length=20)
+	private String pname;
+	
+	private Integer pcountOfRating;
+	
+	private Integer ptotalOfRating;
+	
+	private Double pavgOfRating;
+	
+	private Integer pstock;
+	
+	private Integer pprice;
+	
+	private Boolean papproval;
+	
+	@OneToMany(mappedBy="product", cascade= {CascadeType.PERSIST, CascadeType.REMOVE})
+	private Set<Review> reviews;
+	
+	@ManyToOne
+	@JoinColumn(name="ctgno")
+	private Integer ctgno;
+	
+	@PrePersist
+	public void init() {
+		this.pcountOfRating = 0;
+		this.ptotalOfRating = 0;
+		this.pavgOfRating = 0.0;
+		this.pstock = 0;
+		this.pprice = 0;
+	}
+	
+	public void addReview(Review review) {
+		if(this.reviews==null)
+			this.reviews = new HashSet<>();
+		reviews.add(review);
+		this.pcountOfRating++;
+		this.ptotalOfRating+=review.getRating();
+		this.pavgOfRating=this.ptotalOfRating/(double)this.pcountOfRating;	
+	}
+}
---
+++ TeamPro/src/main/java/com/example/plantforu/entity/product/ReviewId.java
@@ -0,0 +1,12 @@
+package com.example.plantforu.entity.product;
+
+import java.io.*;
+
+import lombok.*;
+
+@SuppressWarnings("serial")
+@EqualsAndHashCode
+public class ReviewId implements Serializable  {
+	private Integer product;
+	private Integer rno;
+}
