5a0f1eca6d669ff5e181db292c432a145d860726
MrCharon286 <sungupark83@gmail.com>
2021-10-21 16:33:25 +0900

    상품 이미지, 문의 추가, 리뷰 Repository 추가

---
+++ TeamPro/src/main/java/com/example/plantforu/repository/ProductImageRepository.java
@@ -0,0 +1,13 @@
+package com.example.plantforu.repository;
+
+import java.util.*;
+
+import org.springframework.data.jpa.repository.*;
+
+import com.example.plantforu.entity.product.*;
+
+public interface ProductImageRepository extends JpaRepository<ProductImage, ProductImageId> {
+
+	List<ProductImage> findByPnoOrderByPimagenoDesc(Integer pno);
+
+}
